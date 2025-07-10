d58c2957ca47f21d573fcc6051f56be9cabcc93f
Italo <italomatos764@gmail.com>
2025-07-09 23:35:49 -0300

    feat: add drawer navigation to Appbar component for improved user access

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,17 +1,23 @@
+"use client";
 import { Menu } from "@mui/icons-material";
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Drawer, IconButton, Toolbar, Typography } from "@mui/material";
+import { useState } from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
+import TerminalIcon from "@mui/icons-material/Terminal";
 
 export default function Appbar() {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const toggleDrawer = () => {
+    setOpenDrawer(!openDrawer);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }} component={"div"}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={toggleDrawer}>
             <Menu />
           </IconButton>
           <Typography variant="h6" color="inherit" component="div">
@@ -19,6 +25,25 @@ export default function Appbar() {
           </Typography>
         </Toolbar>
       </AppBar>
+
+      <Drawer anchor="left" open={openDrawer} onClose={toggleDrawer}>
+        <Typography variant="h6" sx={{ padding: 2 }}>
+          Menu UDLF Web
+        </Typography>
+        <Box sx={{ width: 300 }} role="presentation" onClick={toggleDrawer} onKeyDown={toggleDrawer}>
+          <Box sx={{ padding: 2 }}>
+            <Button variant="text" startIcon={<HomeIcon />} onClick={() => (window.location.href = "/")}>
+              Home
+            </Button>
+            <Button variant="text" onClick={() => (window.location.href = "/get-started")} startIcon={<NoteAddIcon />}>
+              Create new config file
+            </Button>
+            <Button variant="text" onClick={() => (window.location.href = "/load-config")} startIcon={<TerminalIcon />}>
+              Run config file
+            </Button>
+          </Box>
+        </Box>
+      </Drawer>
     </Box>
   );
 }
