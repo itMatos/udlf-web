5f2b428c4cab55fb6a25e567fe37ccd8440c08af
Italo <italomatos764@gmail.com>
2025-08-18 01:50:44 -0300

    feat: enhance image results display with card layout and improve pagination handling

--- src/app/result/[outputname]/page.tsx
+++ src/app/result/[outputname]/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Box, FormControl, InputLabel, MenuItem, Pagination, Select, type SelectChangeEvent, Typography } from "@mui/material";
+import { Box, Card, CardHeader, CardMedia, FormControl, InputLabel, MenuItem, Pagination, Select, type SelectChangeEvent, Typography } from "@mui/material";
 import { useParams } from "next/navigation";
 import type React from "react";
 import { useEffect, useState } from "react";
 import Appbar from "@/components/Appbar";
+import type { lineContent } from "@/services/api/models";
 import { getPaginatedListFilenames } from "@/services/api/UDLF-api";
 import { IMAGES_PER_PAGE_DEFAULT } from "@/ts/constants/common";
 
@@ -16,12 +17,14 @@ export default function Result() {
   const [page, setPage] = useState<number>(1);
   const [pageSize, setPageSize] = useState<number>(IMAGES_PER_PAGE_DEFAULT);
   const [totalPages, setTotalPages] = useState<number>(1);
+  const [imagesCurrentPage, setImagesCurrentPage] = useState<lineContent[]>([]);
 
   useEffect(() => {
     const fetchImageNames = async () => {
       try {
         const imageName = await getPaginatedListFilenames(outputname, page, pageSize);
         setTotalPages(imageName.totalPages);
+        setImagesCurrentPage(imageName.items);
       } catch (error) {
         // eslint-disable-next-line no-console
         console.error(`Error fetching image names for output ${outputname}:`, error);
@@ -36,9 +39,7 @@ export default function Result() {
     setPage(1);
   };
 
-  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    console.log("Event", event);
-    console.log("Page changed to:", value);
+  const handlePageChange = (_event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
   };
 
@@ -51,10 +52,31 @@ export default function Result() {
           {` ${outputname}`}
         </Typography>
       </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "flex-start",
+        }}
+      >
+        {imagesCurrentPage.map((image) => {
+          const imageName = image.content;
+          return (
+            <Card key={image.content} sx={{ p: 1, m: 1, width: 150, cursor: "pointer" }}>
+              <CardHeader subheader={`${imageName}`} />
+              <CardMedia
+                alt={`${imageName}`}
+                component="img"
+                image={`http://localhost:8080/image-file/${imageName}`}
+                // onClick={() => filterBySelectedImage(imageIndex)}
+              />
+            </Card>
+          );
+        })}
+      </Box>
       <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", mt: 4, mb: 2, gap: 2 }}>
         <Pagination color="primary" count={totalPages} onChange={handlePageChange} page={page} />
-
-        {/* NOVO: Seletor de Itens por Página */}
         <FormControl size="small" sx={{ m: 1, minWidth: 120 }}>
           <InputLabel id="page-size-select-label">Itens por pág.</InputLabel>
           <Select
