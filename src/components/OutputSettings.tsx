3c74e5b6ae2acd95b50e5ce99ae1fbd693674a44
itMatos <italomatos764@gmail.com>
2025-09-29 23:11:40 -0300

    refactor: update OutputSettings component to enable output by default and simplify output format selection

--- src/components/OutputSettings.tsx
+++ src/components/OutputSettings.tsx
@@ -5,16 +5,8 @@ import type { OutputSettingsProps } from "@/ts/interfaces/output";
 import type { OutputFormatType } from "@/ts/types/output";
 
 export default function OutputSettings({ onSettingsChange, settings }: OutputSettingsProps) {
-  const [enabledOutput, setEnabledOutput] = useState<boolean>(false);
+  const [enabledOutput, setEnabledOutput] = useState<boolean>(true);
   const [selectedOutputType, setSelectedOutputType] = useState<OutputFormatType>(settings.outputFileFormat || "RANKEDLIST_NUMERIC");
-
-  const handleChangeEnabledOutput = (value: boolean) => {
-    setEnabledOutput(value);
-    onSettingsChange({
-      ...settings,
-      enabledOutput: value,
-    });
-  };
 
   const handleOutputFileFormatChange = (value: OutputFormatType) => {
     console.log("Output file format changed:", value);
@@ -35,33 +27,31 @@ export default function OutputSettings({ onSettingsChange, settings }: OutputSet
       <Box autoComplete="off" component="form" noValidate sx={{ gap: 2, display: "flex", flexDirection: "column" }}>
         <FormGroup>
           <FormControlLabel
-            control={<Switch checked={enabledOutput} onChange={(_, checked) => handleChangeEnabledOutput(checked)} />}
+            control={<Switch checked={enabledOutput} disabled />}
             label="Enable output"
             labelPlacement="start"
           />
         </FormGroup>
 
-        {enabledOutput && (
-          <Box sx={{ mb: 2 }}>
-            <TextField
-              fullWidth
-              id="inputType"
-              label="Output format"
-              onChange={(e) => {
-                handleOutputFileFormatChange(e.target.value as OutputFormatType);
-              }}
-              select
-              value={selectedOutputType}
-              variant="outlined"
-            >
-              {OUTPUT_TYPES.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Box>
-        )}
+        <Box sx={{ mb: 2 }}>
+          <TextField
+            fullWidth
+            id="inputType"
+            label="Output format"
+            onChange={(e) => {
+              handleOutputFileFormatChange(e.target.value as OutputFormatType);
+            }}
+            select
+            value={selectedOutputType}
+            variant="outlined"
+          >
+            {OUTPUT_TYPES.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
       </Box>
     </Box>
   );
